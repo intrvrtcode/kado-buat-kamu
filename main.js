7a22d0c82baf08d6de3d97dd26032f5bc3ece427
Aldi Rahmaddani <Kudahitamnfc12@gmail.com>
2023-01-10 23:31:56 +0700

    menambahkan animasi

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const span = document.querySelectorAll('span');
+
+let condition = true;
+
+function animation () {
+  one.style.visibility = 'hidden';
+  two.style.visibility = 'hidden';
+  three.style.visibility = 'hidden';
+  for(let i = 0; i < span.length; i++) {
+    span[i].style.color = '#A31ACB'
+  }
+
+  setTimeout(function() {
+    one.style.visibility = 'unset';
+  }, 500);
+
+  setTimeout(function() {
+    two.style.visibility = 'unset';
+  }, 1000);
+
+  setTimeout(function() {
+    three.style.visibility = 'unset';
+    for(let i = 0; i < span.length; i++) {
+      span[i].style.color = 'magenta'
+    }
+  }, 1500)
+
+  if(condition) {
+    setTimeout(animation, 2200)
+  }
+}
+
+animation();
